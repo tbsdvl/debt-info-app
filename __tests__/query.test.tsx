3af987ea386497a6d58f8bf29c9b6849b53bf1f2
Replit user <>
2023-08-22 20:03:11 +0000

    2-TB Add success/fail unit tests for retrieving debt info by date

--- __tests__/query.test.tsx
+++ __tests__/query.test.tsx
@@ -3,8 +3,8 @@
  * @vitest-environment-options {"url": "https://www.treasurydirect.gov/"}
  */
 import { renderHook, waitFor } from '@testing-library/react';
-import {QueryClient, QueryClientProvider} from 'react-query';
-import {getCurrentDebtQuery} from '../src/queries';
+import {QueryClient, QueryClientProvider, UseQueryResult} from 'react-query';
+import {getCurrentDebtQuery, getDebtByDateQuery} from '../src/queries';
 import {describe, expect, test} from 'vitest';
 
 describe('query', () => {
@@ -15,10 +15,34 @@ describe('query', () => {
     </QueryClientProvider>
   );
 
+  const getQueryResult = (query: UseQueryResult<any, unknown>, ...args) => {
+    console.log(...args);
+    return renderHook(() => query(...args), { wrapper });
+  }
+
   test('should successfully retrieve the current debt', async () => {
-    const { result } = renderHook(() => getCurrentDebtQuery(), { wrapper });
+    const { result } = getQueryResult(getCurrentDebtQuery);
     await waitFor(() => expect(result.current.isSuccess).toBe(true));
     await waitFor(() => expect(result.current.data).toBeDefined());
     await waitFor(() => expect(result.current.data.governmentHoldings).toBeDefined());
+    await waitFor(() => expect(result.current.data.publicDebt).toBeDefined());
+    await waitFor(() => expect(result.current.data.totalDebt).toBeDefined());
+    await waitFor(() => expect(result.current.data.effectiveDate).toBeDefined());
+  });
+
+  test('should successfully retrieve the debt information for a valid date', async () => {
+   const { result } = getQueryResult(getDebtByDateQuery, '2018-04-13 19:18');
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    await waitFor(() => expect(result.current.data).toBeDefined());
+    await waitFor(() => expect(result.current.data.governmentHoldings).toBe(5667057298759.77));
+    await waitFor(() => expect(result.current.data.publicDebt).toBe(15396372854511.25));
+    await waitFor(() => expect(result.current.data.totalDebt).toBe(21063430153271.02));
+    await waitFor(() => expect(result.current.data.effectiveDate).toBe('April 13, 2018 EDT'));
+  });
+
+  test('should fail to retrieve the debt information for a invalid date', async () => {
+   const { result } = getQueryResult(getDebtByDateQuery, 'not a date');
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    await waitFor(() => expect(result.current.data).toBeUndefined());
   });
 });
--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,6 +1,10 @@
 import {useQuery} from 'react-query';
-import {getCurrentDebt} from '../api';
+import {getCurrentDebt, getDebtByDate} from '../api';
 
 export const getCurrentDebtQuery = () => {
   return useQuery({queryKey: ['current'], queryFn: getCurrentDebt});
+}
+
+export const getDebtByDateQuery = (date: string | Date) => {
+  return useQuery({queryKey: ['byDate'], queryFn: () => getDebtByDate(date)});
 }
